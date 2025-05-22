9e7ca56510cb5112d2b59fd8b51b5aadc750b36d
nanom80 <nanomegi@gmail.com>
2025-05-22 17:49:20 +0900

    2025-05-22 17:49

--- Ex/day01.sql
+++ Ex/day01.sql
@@ -28,6 +28,14 @@ select	employee_id as '사번',
 from	employees
 ;
 
+select	concat(first_name,'-',last_name) as '성명',
+		salary as '월급',
+		salary*12 as '연봉',
+        salary*12+5000 as '보너스',
+        phone_number as '전화번호'
+from	employees
+;
+
 
 
 
