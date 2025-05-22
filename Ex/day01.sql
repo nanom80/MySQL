40e89ea1510811a2c9b90f10068839fa03c35bfd
nanom80 <nanomegi@gmail.com>
2025-05-22 17:43:14 +0900

    2025-05-22 17:43

---
+++ Ex/day01.sql
@@ -0,0 +1,62 @@
+show databases;
+show schemas;
+show tables;
+select * from information_schema.tables where table_schema = 'hr_db';
+select * from information_schema.columns where table_name = 'countries';
+show columns from employees;
+-- 주석
+/* 주석 */
+
+select * from countries;		-- 나라
+select * from departments;	-- 부서
+select * from employees;		-- 직원
+select * from job_history;	-- 업무현황
+select * from jobs;			-- 업무
+select * from locations;		-- 도시
+select * from regions;		-- 지역
+
+select	employee_id as '사번',
+		first_name as '이름',
+        last_name as '성',
+        concat(first_name, ' ',last_name, ' hire date is ', hire_date) as 'name',
+        salary as '월급',
+        salary * 12 as '연봉',
+        phone_number as '전화번호',
+        email as '이메일',
+        hire_date as '입사일',
+        first_name * 10	as '연산시 문자열은 0으로 처리'
+from	employees
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
