b38ce567ad1d6b6cadae80b611c54626557178d2
nanom80 <nanomegi@gmail.com>
2025-05-29 09:40:17 +0900

    2025-05-29 09:40

--- Ex/day06.sql
+++ Ex/day06.sql
@@ -1,2 +1,25 @@
 /* 2025-05-29 */
 
+/*
+use hr_db;
+
+show databases;
+show schemas;
+show tables;
+select * from information_schema.tables where table_schema = 'hr_db';
+select * from information_schema.columns where table_name = 'countries';
+show columns from employees;
+show columns from job_history;
+show columns from jobs;
+show columns from departments;
+show full processlist; -- 현재 실행 중인 모든 스레드/세션 목록 (= select * from information_schema.processlist);
+kill 51; -- 특정 쿼리 종료 (kill Id)
+*/
+
+# 조인 연습(equi join, inner join 두가지로 풀어볼것)
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명
+   
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명, 나라아이디, 나라명
+   
+   
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명, 나라아이디, 나라명, 지역아이디, 지역명
