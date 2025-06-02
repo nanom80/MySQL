0843e5edf4d0f4a84b2a624fba1800d0e6bbdc61
nanom80 <nanomegi@gmail.com>
2025-06-02 17:29:37 +0900

    2025-06-02 17:29

---
+++ Ex/day09_web.sql
@@ -0,0 +1,26 @@
+/* 2025-06-03 (화) */
+
+/*
+use web_db;
+
+show databases;
+show schemas;
+show tables;
+select * from information_schema.tables where table_schema = 'hr_db';
+select * from information_schema.columns where table_name = 'countries';
+show columns from employees;
+show full processlist; -- 현재 실행 중인 모든 스레드/세션 목록 (= select * from information_schema.processlist);
+kill 51; -- 특정 쿼리 종료 (kill Id)
+
+select user(); -- USER(): 클라이언트가 인증에 사용한 계정.
+select current_user(); -- 현재 세션에서 사용되는 계정(권한 기준 계정)
+
+-- 현재 세션의 트랜잭션 상태 확인(root)
+SELECT * FROM information_schema.innodb_trx;
+
+*/
+
+
+
+
+
